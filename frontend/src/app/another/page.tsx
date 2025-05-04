5e7ecf9cd5762cdd87ed1bf31503917f2d436baf
erdem koyuncu <erdemfb1907@outlook.com>
2025-05-04 21:43:49 +0300

    Added shadcn, experimenting with file based routing

---
+++ frontend/src/app/another/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function Another() {
+  return (
+    <>
+      Hello
+      <Button>Click Me</Button>
+    </>
+  );
+}
