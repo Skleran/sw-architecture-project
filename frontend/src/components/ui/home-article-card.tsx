19222e60faf1cae730165738beee97f25fff25d7
erdem koyuncu <erdemfb1907@outlook.com>
2025-05-20 19:46:21 +0300

    Adjusted article card, is now ready, will move on to side-bar content now

--- frontend/src/components/ui/home-article-card.tsx
+++ frontend/src/components/ui/home-article-card.tsx
@@ -15,14 +15,21 @@ type Article = {
 export default function HomeArticleCard({ data }: { data: Article }) {
   return (
     <div className="w-full flex flex-col items-center justify-center">
-      <div className="w-full max-w-[680px] flex flex-col gap-3 mt-8">
+      <Link
+        href={`/${data.author.name}/${data.title}`}
+        className="w-full max-w-[680px] flex flex-col gap-3 mt-8 mx-6"
+      >
         <div className="flex items-center gap-2">
-          <img
-            src={data.author.image}
-            alt={data.author.name}
-            className="size-6 rounded-full"
-          />
-          <Link href={"/"}>
+          <Link href={`/${data.author.name}`}>
+            {" "}
+            <img
+              src={data.author.image}
+              alt={data.author.name}
+              className="size-6 rounded-full hover:brightness-90"
+            />
+          </Link>
+
+          <Link href={`/${data.author.name}`}>
             <Button
               variant={"link"}
               size={"sm"}
@@ -35,23 +42,39 @@ export default function HomeArticleCard({ data }: { data: Article }) {
         </div>
 
         <div className="w-full grid grid-cols-[5fr_2fr] grid-rows-[auto_auto]">
-          <div className="w-full flex flex-col gap-1.5">
-            <h2 className="font-bold text-xl md:text-2xl leading-[1.15] md:leading-[1.25] md:tracking-[-0.01em] line-clamp-4 overflow-visible">
-              {data.title}
-            </h2>
-            <div className="line-clamp-2 text-muted-foreground">
-              {data.excerpt}
+          <div className="w-full flex flex-col justify-between gap-5">
+            <div className="flex flex-col gap-2.5">
+              <h2 className="font-bold text-xl md:text-2xl leading-[1.15] md:leading-[1.25] md:tracking-[-0.01em] line-clamp-4 overflow-visible">
+                {data.title}
+              </h2>
+              <div className="line-clamp-2 text-muted-foreground leading-5">
+                {data.excerpt}
+              </div>
+            </div>
+
+            <div className="hidden md:flex items-center gap-5 text-muted-foreground text-[13px]">
+              <p>{data.release}</p>
+              <div className="flex gap-1 items-center">
+                <ThumbsUp size={15} />
+                <p>{data.likes}</p>
+              </div>
+              <div className="flex gap-1 items-center">
+                <MessageCircle size={15} />
+                <p>{data.comments}</p>
+              </div>
             </div>
           </div>
-          <div className="w-full items-center flex pl-4 md:pl-10">
-            <img
-              src={data.coverImage}
-              alt="cover"
-              className="object-contain rounded-sm"
-            />
+          <div className="w-full items-center flex pl-4 md:pl-10 justify-center">
+            <div className="">
+              <img
+                src={data.coverImage}
+                alt="cover"
+                className="object-contain md:object-cover rounded-sm max-h-[100px] md:max-h-[120px] md:min-w-[120px]"
+              />
+            </div>
           </div>
 
-          <div className="w-full flex justify-between mt-4 col-span-2 md:col-span-1">
+          <div className="w-full md:hidden flex justify-between mt-4 col-span-2 md:col-span-1">
             <div className="flex items-center gap-5 text-muted-foreground text-[13px]">
               <p>{data.release}</p>
               <div className="flex gap-1 items-center">
@@ -66,7 +89,7 @@ export default function HomeArticleCard({ data }: { data: Article }) {
             <div className=""></div>
           </div>
         </div>
-      </div>
+      </Link>
       <div className="w-full max-w-[680px] border-b mt-8" />
     </div>
   );
