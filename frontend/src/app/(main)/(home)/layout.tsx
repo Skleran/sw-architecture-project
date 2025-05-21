1fcabcdc56c48937800f1b6231a496e0ddabde3a
erdem koyuncu <erdemfb1907@outlook.com>
2025-05-21 18:43:24 +0300

    Added tag selector component

---
+++ frontend/src/app/(main)/(home)/layout.tsx
@@ -0,0 +1,30 @@
+import TagSelector from "@/components/tag-selector";
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const tags = [
+    { id: "1", name: "Programming", slug: "programming" },
+    { id: "2", name: "Machine Learning", slug: "machine-learning" },
+    { id: "3", name: "Data Science", slug: "data-science" },
+    { id: "4", name: "Web Development", slug: "web-development" },
+    { id: "5", name: "Humor", slug: "humor" },
+    { id: "6", name: "UX", slug: "ux" },
+    { id: "7", name: "React", slug: "react" },
+  ];
+  return (
+    <>
+      <div className="w-full lg:w-[1200px] grid grid-cols-[1fr] lg:grid-cols-[2fr_1fr] ">
+        <div className="flex flex-col relative">
+          <div className="w-full flex justify-center absolute">
+            <TagSelector tags={tags} />
+          </div>
+          <div className="mx-6 mt-[61px]">{children}</div>
+        </div>
+        <div className="flex-col gap-4 border-2 border-blue-600/30 hidden lg:flex"></div>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/app/(main)/(home)/page.tsx
@@ -0,0 +1,12 @@
+import HomeArticleCard from "@/components/ui/home-article-card";
+import articles from "../mockup_articles.json";
+
+export default function Home() {
+  return (
+    <>
+      {articles.map((article, index) => (
+        <HomeArticleCard key={index} data={article} />
+      ))}
+    </>
+  );
+}
